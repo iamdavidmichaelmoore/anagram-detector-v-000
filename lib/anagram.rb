19453be96f52fda1e3779751a39e1f210946814a
David Moore <hey@iamdavidmoore.com>
2018-05-07 17:24:05 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(anagrams_ary)
+    temp = []
+    puts anagrams_ary
+    anagrams_ary.each do |word|
+      ary_word = word.split("").sort
+      init_word = anagram.split("").sort
+      temp = ary_word == init_word ? word : []
+    end
+    temp
+  end
+end
