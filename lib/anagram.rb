84e62fc4ec5d4800e417e92df7b8ada4230a54c2
David Moore <hey@iamdavidmoore.com>
2018-05-07 17:32:55 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,4 @@
-# Your code goes here!
-
+require 'pry'
 class Anagram
 
   attr_accessor :anagram
@@ -13,6 +12,7 @@ class Anagram
     anagrams_ary.each do |word|
       ary_word = word.split("").sort
       init_word = anagram.split("").sort
+      binding.pry
       temp = ary_word == init_word ? anagram : []
     end
     temp
